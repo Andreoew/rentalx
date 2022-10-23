607a818956fa6687ea3431c49373a10878b4d9a9
André de Souza <andreoew@hotmail.com>
2022-10-23 18:24:09 -0300

    update upload and list specifications

--- src/routes/specifications.routes.ts
+++ src/routes/specifications.routes.ts
@@ -1,26 +1,15 @@
 import { Router } from "express";
-import { SpecificationsRepository } from "../modules/cars/repositories/SpecificationsRepository";
-import { CreateSpecificationService } from "../modules/cars/services/CreateSpecificationsService";
+import { createSpecificationController } from "../modules/cars/useCases/createSpecification";
+import { listSpecificationsController } from "../modules/cars/useCases/listSpecifications";
 
 const specificationsRoutes = Router();
 
-const specificationsRepository = new SpecificationsRepository();
-
 specificationsRoutes.post("/", (request, response) => {
-  const { name, description } = request.body;
-  const createSpecifictionsService = new CreateSpecificationService(
-    specificationsRepository
-  );
-
-  createSpecifictionsService.execute({ name, description });
-
-  return response.status(201).send();
+  return createSpecificationController.habdle(request, response);
 });
 
 specificationsRoutes.get("/", (request, response) => {
-  const all = specificationsRepository.list();
-
-  return response.json(all);
+  return listSpecificationsController.handle(request, response);
 });
 
 export { specificationsRoutes };
